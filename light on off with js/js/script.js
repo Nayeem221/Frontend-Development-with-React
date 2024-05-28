75f9e8b82410001984116fd66a518a78e4228fca
Nayeem221 <103898592+Nayeem221@users.noreply.github.com>
2024-05-28 23:25:55 +0600

    Add files via upload

---
+++ light on off with js/js/script.js
@@ -0,0 +1,10 @@
+function bulbon(){
+    let bulbone= document.querySelector(".bulboff")
+    bulbone.setAttribute("src","images/lighton.jpg");
+}
+
+function bulboff(){
+    let bulbtwo= document.querySelector(".bulboff")
+    bulbtwo.setAttribute("src","images/lightoff.jpg");
+}
+
