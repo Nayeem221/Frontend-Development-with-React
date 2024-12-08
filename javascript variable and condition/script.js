7f78ed584d987ce2e846d22c7ae1709dbbdba9d1
Nayeem221 <103898592+Nayeem221@users.noreply.github.com>
2024-12-08 20:29:18 +0600

    commit

---
+++ javascript variable and condition/script.js
@@ -0,0 +1,32 @@
+// // ==================Var will replace the elements current value we will get the last value of the var ===============================
+// // var a=56 
+// // var a=67
+// // var a=76
+// // console.log(a); 
+// //======================================= value of the let can be reassign but we cannot declare let again ========================================
+// let a=65;
+//  a=677
+//  console.log(a); 
+
+// // ==============================value of the const cannot be reassign=====================================
+// //  const argument='nayeem'
+// // console.log(argument);
+
+
+// it will give alert message whenver we aenter the brower or give this command 
+// alert('milon')
+///prompt will give you a search box and  ok same as alert 
+// let age=prompt("how old are you")
+// console.log(age);
+// let job=prompt('ki koro ')
+//  if(job=='kisuna'){
+//     alert('start earning money first')
+//  }else{
+//   let a=prompt('how much you earn monthly')
+//   if(a>=25000){
+//  alert('you are good you can have sex with my daughter ')
+
+//   }else{
+//     alert('rejected')
+//   }
+//  }
