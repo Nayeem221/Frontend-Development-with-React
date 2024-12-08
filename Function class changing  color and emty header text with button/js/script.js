84a34d2feb8db6b8e6fa27484a6f2ad8489f9922
Nayeem221 <103898592+Nayeem221@users.noreply.github.com>
2024-12-08 20:28:33 +0600

    commit

---
+++ Function class changing  color and emty header text with button/js/script.js
@@ -0,0 +1,11 @@
+const main=document.querySelector('.main')
+const header=document.querySelector('.header')
+const button=document.querySelector('.button')
+
+
+button.addEventListener('click',()=>{
+  
+    header.innerHTML='KI KHOBOR '
+     main.style.background="yellow"
+    
+})
